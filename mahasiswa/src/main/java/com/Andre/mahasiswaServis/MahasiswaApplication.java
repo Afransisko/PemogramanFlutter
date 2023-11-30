ebaad16885d34836d15bb9dfecf575d465610ad1
hyfizh <afransisko03@gmail.com>
2023-11-30 23:48:40 +0700

    .

---
+++ mahasiswa/src/main/java/com/Andre/mahasiswaServis/MahasiswaApplication.java
@@ -0,0 +1,13 @@
+package com.Andre.mahasiswaServis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahasiswaApplication.class, args);
+	}
+
+}
